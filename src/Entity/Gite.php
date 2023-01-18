9b88554ec4e1f7f8873704261d7d71738b4024fe
Julien <julien.dousse@gmail.com>
2023-01-18 14:45:56 +0100

    Start/END red Fonctionnel + services/eqp Fonctionnel

--- src/Entity/Gite.php
+++ src/Entity/Gite.php
@@ -457,7 +457,7 @@ class Gite
         if (null !== $imageFile) {
             // It is required that at least one field changes if you are using doctrine
             // otherwise the event listeners won't be called and the file is lost
-            $this->imageUpdatedAt = new \DateTimeImmutable();
+            $this->imageUpdatedAt = new \DateTime();
         }
     }
 
